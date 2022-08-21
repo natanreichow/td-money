5d496ee6ced1f229e22f8af08d3cc4686a5c1854
natanreichow <natanreichow@gmail.com>
2022-08-21 21:59:06 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { Header } from "./components/Header";
+import { Transactions } from "./pages/Transactions";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <Transactions />
+    </ThemeProvider>
+  )
+}
+
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { SummaryCard, SummaryContainer } from "./styles"
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+
+export function Summary() {
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Income</span>
+          <ArrowCircleUp size={32} color="#00B37E"/>
+        </header>
+        <strong>€ 17.400,00</strong>
+      </SummaryCard>
+
+      <SummaryCard>
+        <header>
+          <span>Expenses</span>
+          <ArrowCircleDown size={32} color="#F75A68"/>
+        </header>
+        <strong>€ 12.900,00</strong>
+      </SummaryCard>
+
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF"/>
+        </header>
+        <strong>€ 5.500,00</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
+}
