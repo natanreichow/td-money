edd406e7bef0a898f9ca16a5ca9e4bf6e351ac45
natanreichow <natanreichow@gmail.com>
2022-08-23 15:24:48 +0200

    ContextAPI, summary and server json added

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,30 @@
 import { SummaryCard, SummaryContainer } from "./styles"
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useContext } from "react"
+import { TransactionContext } from "../../contexts/TransactionsContext"
 
 export function Summary() {
+  const { transactions } = useContext(TransactionContext)
+
+  // reduce transactions to { income: 0, expenses: 0, balance: 0 }
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price
+      acc.balance += transaction.price
+    } else {
+      acc.expenses += transaction.price
+      acc.balance -= transaction.price
+    }
+
+    return acc
+  },
+  {
+    income: 0,
+    expenses: 0,
+    balance: 0
+  }
+  )
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -9,7 +32,7 @@ export function Summary() {
           <span>Income</span>
           <ArrowCircleUp size={32} color="#00B37E"/>
         </header>
-        <strong>€ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -17,7 +40,7 @@ export function Summary() {
           <span>Expenses</span>
           <ArrowCircleDown size={32} color="#F75A68"/>
         </header>
-        <strong>€ 12.900,00</strong>
+        <strong>{summary.expenses}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -25,7 +48,7 @@ export function Summary() {
           <span>Balance</span>
           <CurrencyDollar size={32} color="#FFF"/>
         </header>
-        <strong>€ 5.500,00</strong>
+        <strong>{summary.balance}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,39 +1,35 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionContext } from "../../contexts/TransactionsContext";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, Transactionstable } from "./styles";
 
 export function Transactions() {
+  const {transactions} = useContext(TransactionContext)
+
   return (
     <div>
       <Header />
       <Summary />
-
       <TransactionsContainer>
         <SearchForm />
-
         <Transactionstable>
           <tbody>
-            <tr>
-              <td width="50%">Development from website</td>
-              <td>
-                <PriceHighlight variant="income">
-                  € 4.000,00
-                </PriceHighlight>
-              </td>
-              <td>Sell</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              <td>
-                <PriceHighlight variant="expense">
-                  - € 12,00
-                </PriceHighlight>
-              </td>
-              <td>Food</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </Transactionstable>
       </TransactionsContainer>
